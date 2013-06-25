eaa5aa151cb08ac351d8a694aacab842c4ac983f
Hector47 <hector@cachoulet.com>
2013-06-26 01:27:20 +0200

    Reduce complexity of Random

--- Agent.Contrib/Util/Random.cs
+++ Agent.Contrib/Util/Random.cs
@@ -3,21 +3,27 @@ using Microsoft.SPOT;
 
 namespace Agent.Contrib.Util
 {
-    
+
     public class Random
     {
         private static System.Random rnd = new System.Random();
 
+        /// <summary>
+        /// Return an int greater or equal at min and less than max
+        /// </summary>
         public static int Next(int min, int max)
         {
+            if (min > max) //if min greater than max we swap them
+            {
+                int temp = max;
+                max = min;
+                min = temp;
+            }
+            
             lock (rnd)
             {
-                int num = rnd.Next(max);
-                while(num < min)
-                {
-                    num = rnd.Next(max);
-                }
-                return num;
+                int num = rnd.Next(max - min);
+                return num + min;
             }
         }
     }

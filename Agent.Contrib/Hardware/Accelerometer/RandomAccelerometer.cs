95bed595de585af1ae14705efed72be7b239512e
Rob Chartier <rob.chartier@gmail.com>
2013-07-05 16:28:28 -0700

    added soft support for a random accelerometer

---
+++ Agent.Contrib/Hardware/Accelerometer/RandomAccelerometer.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Threading;
+using Microsoft.SPOT;
+
+namespace Agent.Contrib.Hardware.Accelerometer
+{
+    public class RandomAccelerometer : IAccelerometer
+    {
+        private Timer randTimer = null;
+
+        public RandomAccelerometer()
+        {
+            NewRandomReading();
+            randTimer = new Timer(NewReading, null, 1000, 1000);
+        }
+        private void NewRandomReading()
+        {
+            CurrentReading = new Reading()
+            {
+                X = rnd.NextDouble()*10,
+                Y = rnd.NextDouble()*10,
+                Z = rnd.NextDouble()*10,
+                TimeStamp = DateTime.Now
+            };
+        }
+        System.Random rnd = new Random();
+        private void NewReading(object state)
+        {
+
+            NewRandomReading();
+            if (OnAccelerometerReading != null) OnAccelerometerReading(CurrentReading);
+            randTimer.Change(1000, rnd.Next(3000));
+        }
+        public Reading CurrentReading { get; set; }
+        public event AccelerometerReading OnAccelerometerReading;
+    }
+}
